327ea230389a13c5008794fe8d151a82dff972f8
Aditya01229 <adityajasoriya2003@gmail.com>
2025-01-31 02:00:45 +0530

    error solving

--- Backend/src/shop/shop.module.ts
+++ Backend/src/shop/shop.module.ts
@@ -1,7 +1,7 @@
 import { Module } from '@nestjs/common';
 import { AboutusController } from './shop.controller';
 import { AboutusService } from './shop.service';
-import { DatabaseModule } from 'src/database/database.module';
+import { DatabaseModule } from '../database/database.module';
 import { CloudinaryService } from '../cloudinary.service';
 import { PassportModule } from '@nestjs/passport';
 import { JwtModule } from '@nestjs/jwt';
