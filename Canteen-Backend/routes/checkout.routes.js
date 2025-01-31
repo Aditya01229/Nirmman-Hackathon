ed2356f55ad52935cc2bf3efa745949b27a6fd5e
Aditya Jasoriya <114353434+Aditya01229@users.noreply.github.com>
2025-01-31 14:40:50 +0530

    final

--- Canteen-Backend/routes/checkout.routes.js
+++
@@ -1,8 +0,0 @@
-// routes/checkout.routes.js
-const express = require('express');
-const router = express.Router();
-const checkoutController = require('../controllers/checkout.controller');
-
-router.post('/', checkoutController.checkoutCart);
-
-module.exports = router;
